699fbdd5f96958a7be38eadfed59e782fdb0fe42
Sachin Hooda <sachinhooda@hotmail.com>
2021-05-02 01:55:36 +0530

    Added favicon icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 const Footer = (props) => {
   return (
     <div className="ui center aligned segment">
+      <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       <footer>&copy; Copyright 2021 Anime Wiki Land </footer>
     </div>
   );
